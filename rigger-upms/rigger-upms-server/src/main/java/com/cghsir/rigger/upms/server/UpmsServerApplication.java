c188fdd6773f5a8bc3af8d3429859b615ae9c79f
cghsir <cghsir@gmail.com>
2017-07-13 10:34:59 +0800

    用户权限管理系统web入口

---
+++ rigger-upms/rigger-upms-server/src/main/java/com/cghsir/rigger/upms/server/UpmsServerApplication.java
@@ -0,0 +1,15 @@
+package com.cghsir.rigger.upms.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户权限管理系统web入口
+ * Created by cghsir on 2017/7/13.
+ */
+@SpringBootApplication
+public class UpmsServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UpmsServerApplication.class, args);
+    }
+}
